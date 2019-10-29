f8ddef04603b0aa543e5f6af96cebbfc3837d126
Stephen Trewick <stephent818@gmail.com>
2019-10-29 12:06:37 -0700

    imported Input to StepOne/AboutYou, form displays but needs more functionality such as inputChangedHandler

--- src/containers/AboutYou/AboutYou.js
+++ src/containers/AboutYou/AboutYou.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import Input from '../../components/UI/Input/Input';
 import classes from './AboutYou.css';
 
 class AboutYou extends Component {
@@ -62,16 +63,48 @@ class AboutYou extends Component {
 				valid: false,
 				touched: false
 			}
+		},
+		formIsValid: false
+	}
+
+	stepOneHandler = (event) => {
+		event.preventDefault();
+		const formData = {};
+		for (let formElementIdentifier in this.state.personalInfo) {
+			formData[formElementIdentifier] = this.state.personalInfo[formElementIdentifier].value;
 		}
 	}
+
 	render() {
+		const formElementsArray = [];
+		for (let key in this.state.personalInfo) {
+			formElementsArray.push({
+				id: key,
+				config: this.state.personalInfo[key]
+			});
+		}
+		let form = (
+			<form onSubmit={this.stepOneHandler}>
+				{formElementsArray.map(formElement => (
+					<Input
+						valueType={formElement.id}
+						elementType={formElement.config.elementType}
+						elementConfig={formElement.config.elementConfig}
+						value={formElement.config.value}
+						key={formElement.id}
+						touched={formElement.config.touched}
+						shouldValidate={formElement.config.validation}
+						invalid={!formElement.config.valid}
+						changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+				))}
+				<button btnType="Success">"Next" Create Button component</button>
+			</form>
+		);
+
 		return(
 			<div className={classes.AboutYou}>
 				<p>Tell us about you</p>
-				<p>Input First Name</p>
-				<p>Input Last Name</p>
-				<p>Input Email address</p>
-				<p>Input phone number</p>
+				{form}
 			</div>
 		);
 	}
