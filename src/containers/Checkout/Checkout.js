cc05da9d6de35ff757854b0317d42c60b1bb6c0b
Stephen Trewick <stephent818@gmail.com>
2019-10-31 23:19:35 -0700

    filled in photos and checkout class

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import classes from './Checkout.css';
+
+export class Checkout extends Component {
+  render() {
+    return (
+      <div className={classes.Checkout}>
+        <p>Getting paid</p>
+      </div>
+    )
+  }
+}
+
+export default Checkout;
--- src/containers/Photos/Photos.js
+++ src/containers/Photos/Photos.js
@@ -2,12 +2,32 @@ import React, { Component } from 'react';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Input/Input';
+import classes from './Photos.css';
 
 export class Photos extends Component {
+  state = {
+
+  }
+
+  previous = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  }
+
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  }
+
   render() {
+    // Assign each element in the form array an id and config
+    let form = {
+      
+    }
     return (
-      <div>
-        
+      <div className={classes.Photos}>
+        <p>Add photos to your TableFlash menu</p>
+        {form}
       </div>
     )
   }
